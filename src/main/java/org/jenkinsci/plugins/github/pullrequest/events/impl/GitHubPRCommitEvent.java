a81ed25f9425108e10755c6cb89904f57d5370e7
Kanstantsin Shautsou <Kanstantsin_Shautsou@epam.com>
2015-04-10 17:04:25 +0300

    Split one event in two. Remove useless iterable.

--- src/main/java/org/jenkinsci/plugins/github/pullrequest/events/impl/GitHubPRCommitEvent.java
+++ src/main/java/org/jenkinsci/plugins/github/pullrequest/events/impl/GitHubPRCommitEvent.java
@@ -1,18 +1,12 @@
 package org.jenkinsci.plugins.github.pullrequest.events.impl;
 
-import com.google.common.collect.Iterables;
 import hudson.Extension;
 import org.jenkinsci.plugins.github.pullrequest.GitHubPRCause;
 import org.jenkinsci.plugins.github.pullrequest.GitHubPRPullRequest;
 import org.jenkinsci.plugins.github.pullrequest.GitHubPRTrigger;
 import org.jenkinsci.plugins.github.pullrequest.events.GitHubPREvent;
 import org.jenkinsci.plugins.github.pullrequest.events.GitHubPREventDescriptor;
-import org.kohsuke.github.GHEventPayload;
-import org.kohsuke.github.GHIssueState;
-import org.kohsuke.github.GHPullRequest;
-import org.kohsuke.github.GHPullRequestCommitDetail;
-import org.kohsuke.github.GitUser;
-import org.kohsuke.github.PagedIterable;
+import org.kohsuke.github.*;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 import java.io.IOException;
@@ -20,27 +14,36 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * When PR opened or commits changed in it
- *
+ * Triggers build when commit hash changed
+
  * @author Kanstantsin Shautsou
  */
-public class GitHubPROpenEvent extends GitHubPREvent {
+public class GitHubPRCommitEvent extends GitHubPREvent {
     private static final Logger LOGGER = Logger.getLogger(GitHubPROpenEvent.class.getName());
+    static final String causeMessage = "Commit changed";
 
     @DataBoundConstructor
-    public GitHubPROpenEvent() {
+    public GitHubPRCommitEvent() {
     }
 
     @Override
     public GitHubPRCause isStateChanged(GitHubPRTrigger gitHubPRTrigger, GHPullRequest remotePR, GitHubPRPullRequest localPR) throws IOException {
         if (remotePR.getState().equals(GHIssueState.CLOSED)) {
+            //TODO check whether push to closed allowed?
             return null; // already closed, nothing to check
         }
 
-        GitHubPRCause cause = null;
-        String causeMessage = "PR opened";
         if (localPR == null) { // new
-            cause = new GitHubPRCause(remotePR, remotePR.getUser(), causeMessage, null, null);
+            return null; // not interesting for this event
+        }
+
+        GitHubPRCause cause = null;
+
+        GHCommitPointer head = remotePR.getHead();
+        if (!localPR.getHeadSha().equals(head.getSha())) {
+            LOGGER.log(Level.FINE, "New commit. Sha: {0} => {1}", new Object[]{localPR.getHeadSha(), head.getSha()});
+            GHUser user = head.getUser();
+            cause = new GitHubPRCause(remotePR, remotePR.getUser(), causeMessage, user.getName(), user.getEmail());
         }
 
         return cause;
@@ -50,7 +53,7 @@ public class GitHubPROpenEvent extends GitHubPREvent {
     public static class DescriptorImpl extends GitHubPREventDescriptor {
         @Override
         public final String getDisplayName() {
-            return "Pull Request Opened";
+            return causeMessage;
         }
     }
 }
