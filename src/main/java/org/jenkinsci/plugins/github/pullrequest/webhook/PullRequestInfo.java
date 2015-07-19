8d06cb073a571057a42368a814583733e717e8e0
MerkushevKirill <lanwen@yandex-team.ru>
2015-07-20 01:08:16 +0300

    rft - use extension point from gh-plugin to get hooks

---
+++ src/main/java/org/jenkinsci/plugins/github/pullrequest/webhook/PullRequestInfo.java
@@ -0,0 +1,23 @@
+package org.jenkinsci.plugins.github.pullrequest.webhook;
+
+/**
+ * Bean class to store main info from payload of webhook 
+ * @author lanwen (Merkushev Kirill)
+ */
+public class PullRequestInfo {
+    private final String repo;
+    private final int num;
+
+    public PullRequestInfo(String repo, int num) {
+        this.repo = repo;
+        this.num = num;
+    }
+
+    public String getRepo() {
+        return repo;
+    }
+
+    public int getNum() {
+        return num;
+    }
+}
