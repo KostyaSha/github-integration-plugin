179e8c39d669c8c84595b109759690eccf535f56
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2015-10-27 02:24:06 +0300

    Hack for workflow that breaks groovy token $SCRIPT feature

--- src/main/java/org/jenkinsci/plugins/github/pullrequest/builders/GitHubPRStatusBuilder.java
+++ src/main/java/org/jenkinsci/plugins/github/pullrequest/builders/GitHubPRStatusBuilder.java
@@ -13,6 +13,7 @@ import jenkins.tasks.SimpleBuildStep;
 import org.jenkinsci.plugins.github.pullrequest.GitHubPRCause;
 import org.jenkinsci.plugins.github.pullrequest.GitHubPRMessage;
 import org.jenkinsci.plugins.github.pullrequest.GitHubPRTrigger;
+import org.jenkinsci.plugins.github.util.JobInfoHelpers;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
 import org.kohsuke.github.GHCommitState;
@@ -21,6 +22,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
 import java.io.IOException;
 
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
@@ -29,8 +31,6 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
  * Sets pr status for build caused by GitHubPRCause
  */
 public class GitHubPRStatusBuilder extends Builder implements SimpleBuildStep {
-    private static final long serialVersionUID = 1L;
-
     private static final Logger LOGGER = LoggerFactory.getLogger(GitHubPRStatusBuilder.class);
 
     public static final GitHubPRMessage DEFAULT_MESSAGE = new GitHubPRMessage("${GITHUB_PR_COND_REF} run started");
@@ -57,17 +57,16 @@ public class GitHubPRStatusBuilder extends Builder implements SimpleBuildStep {
     }
 
     @Override
-    public void perform(Run<?, ?> run, FilePath workspace, Launcher launcher, TaskListener listener)
-            throws InterruptedException, IOException {
+    public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath workspace, @Nonnull Launcher launcher,
+                        @Nonnull TaskListener listener) throws InterruptedException, IOException {
         // No triggers in Run class, but we need it
-        AbstractBuild<?, ?> build = (AbstractBuild<?, ?>) run;
-
-        GitHubPRTrigger trigger = build.getParent().getTrigger(GitHubPRTrigger.class);
+        final GitHubPRTrigger trigger = JobInfoHelpers.triggerFrom(run.getParent(), GitHubPRTrigger.class);
         if (trigger == null) {
+            // silently skip. TODO implement error handler, like in publishers
             return;
         }
 
-        GitHubPRCause cause = build.getCause(GitHubPRCause.class);
+        GitHubPRCause cause = run.getCause(GitHubPRCause.class);
         if (cause == null) {
             return;
         }
@@ -75,19 +74,28 @@ public class GitHubPRStatusBuilder extends Builder implements SimpleBuildStep {
         // GitHub status for commit
         try {
             if (statusMessage != null) {
-                String url = trigger.getDescriptor().getJenkinsURL() + build.getUrl();
+                String url = trigger.getDescriptor().getJenkinsURL() + run.getUrl();
+                String expandedMessage;
+                if (run instanceof AbstractBuild<?, ?>) {
+                    final AbstractBuild<?, ?> build = (AbstractBuild<?, ?>) run;
+                    expandedMessage = statusMessage.expandAll(build, listener);
+                } else {
+                    // Workflow doesn't expand variables with token macro!
+                    expandedMessage = statusMessage.getContent();
+                }
 
-                trigger.getRemoteRepo().createCommitStatus(cause.getHeadSha(),
+                trigger.getRemoteRepo().createCommitStatus(
+                        cause.getHeadSha(),
                         GHCommitState.PENDING,
                         url,
-                        statusMessage.expandAll(build, listener),
-                        build.getParent().getFullName());
+                        expandedMessage,
+                        run.getParent().getFullName()
+                );
             }
         } catch (IOException e) {
             listener.getLogger().println("Can't update build description");
             LOGGER.error("Can't set commit status", e);
         }
-
     }
 
     @Extension
