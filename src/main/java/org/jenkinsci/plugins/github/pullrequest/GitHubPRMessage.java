086539ad1c04449db84ce3372ff61f0039149027
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2015-10-27 03:28:26 +0300

    Hack message content expander.

--- src/main/java/org/jenkinsci/plugins/github/pullrequest/GitHubPRMessage.java
+++ src/main/java/org/jenkinsci/plugins/github/pullrequest/GitHubPRMessage.java
@@ -5,10 +5,12 @@ import hudson.Util;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractDescribableImpl;
 import hudson.model.Descriptor;
+import hudson.model.Run;
 import hudson.model.TaskListener;
 import jenkins.model.Jenkins;
 import org.kohsuke.stapler.DataBoundConstructor;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -16,6 +18,8 @@ import java.util.Collection;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.annotation.CheckForNull;
 
 /**
  * Represents a comment for GitHub that can contain token macros.
@@ -33,45 +37,51 @@ public class GitHubPRMessage extends AbstractDescribableImpl<GitHubPRMessage> {
         this.content = content;
     }
 
-    public String expandAll(AbstractBuild<?, ?> build, TaskListener listener) {
+    /**
+     * Expand all what we can from given run
+     */
+    @CheckForNull
+    public String expandAll(Run<?, ?> run, TaskListener listener) throws IOException, InterruptedException {
         String content = getContent();
         if (content == null || content.length() == 0) {
             return content; // Do nothing for an empty String
         }
-//        // Expand environment variables
-//        String s = build.getEnvironment(listener).expand(content);
-        // Expand build variables
-        content = Util.replaceMacro(content, build.getBuildVariableResolver());
 
-        Jenkins jenkins = Jenkins.getInstance();
-        try {
-            ClassLoader uberClassLoader = Jenkins.getInstance().pluginManager.uberClassLoader;
+        // Expand environment variables
+        content = run.getEnvironment(listener).expand(content);
 
-            if (jenkins.getPlugin("token-macro") != null) {
-                List macros = null;
+        // Expand build variables + token macro if they available
+        if (run instanceof AbstractBuild<?, ?>) {
+            final AbstractBuild<?, ?> build = (AbstractBuild<?, ?>) run;
+            content = Util.replaceMacro(content, build.getBuildVariableResolver());
 
-                //get private macroses like groovy template ${SCRIPT} if available
-                if (jenkins.getPlugin("email-ext") != null) {
-                    Class<?> contentBuilderClazz = uberClassLoader.loadClass("hudson.plugins.emailext.plugins.ContentBuilder");
-                    Method getPrivateMacrosMethod = contentBuilderClazz.getDeclaredMethod("getPrivateMacros");
-                    macros = new ArrayList((Collection) getPrivateMacrosMethod.invoke(null));
+            Jenkins jenkins = Jenkins.getActiveInstance();
+            try {
+                ClassLoader uberClassLoader = jenkins.pluginManager.uberClassLoader;
+
+                if (jenkins.getPlugin("token-macro") != null) {
+                    List macros = null;
+
+                    //get private macroses like groovy template ${SCRIPT} if available
+                    if (jenkins.getPlugin("email-ext") != null) {
+                        Class<?> contentBuilderClazz = uberClassLoader.loadClass("hudson.plugins.emailext.plugins.ContentBuilder");
+                        Method getPrivateMacrosMethod = contentBuilderClazz.getDeclaredMethod("getPrivateMacros");
+                        macros = new ArrayList((Collection) getPrivateMacrosMethod.invoke(null));
+                    }
+
+                    //calls TokenMacro.expand(build, listener, content, false, macros)
+                    Class<?> tokenMacroClazz = uberClassLoader.loadClass("org.jenkinsci.plugins.tokenmacro.TokenMacro");
+                    Method tokenMacroExpand = tokenMacroClazz.getDeclaredMethod("expand", AbstractBuild.class,
+                            TaskListener.class, String.class, boolean.class, List.class);
+
+                    content = (String) tokenMacroExpand.invoke(null, build, listener, content, false, macros);
                 }
-
-                //calls TokenMacro.expand(build, listener, content, false, macros)
-                Class<?> tokenMacroClazz = uberClassLoader.loadClass("org.jenkinsci.plugins.tokenmacro.TokenMacro");
-                Method tokenMacroExpand = tokenMacroClazz.getDeclaredMethod("expand", AbstractBuild.class,
-                        TaskListener.class, String.class, boolean.class, List.class);
-
-                content = (String) tokenMacroExpand.invoke(null, build, listener, content, false, macros);
+            } catch (ClassNotFoundException e) {
+                LOGGER.error("Can't find class", e);
+            } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+                LOGGER.error("Can't evaluate macro", e);
             }
-        } catch (ClassNotFoundException e) {
-            LOGGER.error("Can't find class", e);
-        } catch (NoSuchMethodException e) {
-            LOGGER.error("Can't evaluate macro", e);
-        } catch (InvocationTargetException e) {
-            LOGGER.error("Can't evaluate macro", e);
-        } catch (IllegalAccessException e) {
-            LOGGER.error("Can't evaluate macro", e);
+
         }
 
         return content;
