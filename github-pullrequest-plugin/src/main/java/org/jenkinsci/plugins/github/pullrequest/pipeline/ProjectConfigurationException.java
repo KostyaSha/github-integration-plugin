86eec4c8a25fc9c94f7560a219481c7e32bbcd8e
Sean Gilhooly <sean@onecloudlabs.com>
2016-04-06 22:49:49 -0400

    Add pipeline step to update commit status of pull request

---
+++ github-pullrequest-plugin/src/main/java/org/jenkinsci/plugins/github/pullrequest/pipeline/ProjectConfigurationException.java
@@ -0,0 +1,17 @@
+package org.jenkinsci.plugins.github.pullrequest.pipeline;
+
+/**
+ * Raised when the project is configured improperly. Certain steps require particular
+ * project properties to be enabled or set. If the step cannot find the setting it
+ * needs it will raise this error to indicate the missing or incorrect attribute.
+ */
+public class ProjectConfigurationException extends Exception {
+
+    public ProjectConfigurationException(String message) {
+        super(message);
+    }
+
+    public ProjectConfigurationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
