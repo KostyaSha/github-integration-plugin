7c6014841f48bc8332af48bb34dc2b77b7b0f3ca
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-06-24 02:53:16 +0300

    try enable ITs

--- travis.sh
+++ travis.sh
@@ -7,4 +7,4 @@ sudo apt-get update && sudo apt-get install oracle-java8-installer
 java -version
 
 
-mvn -Dignore.random.failures=true clean verify -Ptravis  | grep -v Download
+mvn -Dignore.random.failures=true clean verify -Pgithub-its  | grep -v Download
