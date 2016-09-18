3fbaa72c36bf4cd58dff7e2727f1238f40a4be18
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-09-18 21:20:24 +0300

    Wip unused fields.
    Signed-off-by: Kanstantsin Shautsou <kanstantsin.sha@gmail.com>

--- github-pullrequest-plugin/src/main/resources/org/jenkinsci/plugins/github/pullrequest/GitHubPRTrigger/global.groovy
+++ github-pullrequest-plugin/src/main/resources/org/jenkinsci/plugins/github/pullrequest/GitHubPRTrigger/global.groovy
@@ -8,12 +8,4 @@ f.section(title: _(descriptor.displayName)) {
     f.entry(title: _("Published Jenkins URL"), field: "publishedURL") {
         f.textbox()
     }
-
-    f.entry(title: _("Add user to white list phrase"), field: "whitelistUserMsg") {
-        f.textbox(default: ".*ok\\W+to\\W+test.*")
-    }
-
-    f.entry(title: _("Crontab line"), field: "spec", help: "/descriptor/hudson.triggers.TimerTrigger/help/spec") {
-        f.textbox(default: "H/5 * * * *", checkUrl: "'descriptorByName/hudson.triggers.TimerTrigger/checkSpec?value=' + encodeURIComponent(this.value)")
-    }
 }
