4b7911608e7fe2c37b00fc93c1149389a779a432
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-10-14 03:18:13 +0300

    Missing skip function.

---
+++ github-pullrequest-plugin/src/main/java/org/jenkinsci/plugins/github/pullrequest/trigger/check/SkipPRInBadState.java
@@ -0,0 +1,42 @@
+package org.jenkinsci.plugins.github.pullrequest.trigger.check;
+
+import com.google.common.base.Predicate;
+import org.jenkinsci.plugins.github.pullrequest.GitHubPRPullRequest;
+import org.jenkinsci.plugins.github.pullrequest.GitHubPRRepository;
+import org.jenkinsci.plugins.github.pullrequest.utils.LoggingTaskListenerWrapper;
+import org.kohsuke.github.GHPullRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.CheckForNull;
+
+/**
+ * @author Kanstantsin Shautsou
+ */
+public class SkipPRInBadState implements Predicate<GHPullRequest> {
+    private static final Logger LOG = LoggerFactory.getLogger(NotUpdatedPRFilter.class);
+
+    private final GitHubPRRepository localRepo;
+    private final LoggingTaskListenerWrapper logger;
+
+    private SkipPRInBadState(GitHubPRRepository localRepo, LoggingTaskListenerWrapper logger) {
+        this.localRepo = localRepo;
+        this.logger = logger;
+    }
+
+    public static SkipPRInBadState inBadState(GitHubPRRepository localRepo, LoggingTaskListenerWrapper logger) {
+        return new SkipPRInBadState(localRepo, logger);
+    }
+
+    @Override
+    public boolean apply(GHPullRequest remotePR) {
+        @CheckForNull GitHubPRPullRequest localPR = localRepo.getPulls().get(remotePR.getNumber());
+
+        if (localPR != null && localPR.isInBadState()) {
+            logger.error("local PR [#{} {}] is in bad state", remotePR.getNumber(), remotePR.getTitle());
+            return false;
+        }
+
+        return true;
+    }
+}
