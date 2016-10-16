aef964566e37202292434fb8b53bceceb9f2f4ad
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-10-16 22:57:36 +0300

    Test
    Signed-off-by: Kanstantsin Shautsou <kanstantsin.sha@gmail.com>

---
+++ github-pullrequest-plugin/src/test/java/org/jenkinsci/plugins/github/pullrequest/events/impl/GitHubPRSkipMatchNumberTest.java
@@ -0,0 +1,85 @@
+package org.jenkinsci.plugins.github.pullrequest.events.impl;
+
+import hudson.model.TaskListener;
+import org.jenkinsci.plugins.github.pullrequest.GitHubPRCause;
+import org.jenkinsci.plugins.github.pullrequest.GitHubPRLabel;
+import org.jenkinsci.plugins.github.pullrequest.GitHubPRPullRequest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kohsuke.github.GHCommitPointer;
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueState;
+import org.kohsuke.github.GHLabel;
+import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GHUser;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Collections;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Kanstantsin Shautsou
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class GitHubPRSkipMatchNumberTest {
+    @Mock
+    private GHPullRequest remotePr;
+    @Mock
+    private GitHubPRPullRequest localPR;
+    @Mock
+    private TaskListener listener;
+    @Mock
+    private GHUser mockUser;
+    @Mock
+    private GHCommitPointer mockPointer;
+
+    @Mock
+    private GitHubPRLabel labels;
+    @Mock
+    private GHRepository repository;
+    @Mock
+    private GHIssue issue;
+    @Mock
+    private GHLabel label;
+    @Mock
+    private PrintStream logger;
+
+
+    @Test
+    public void nullNumber() throws IOException {
+        commonExpectations();
+        causeCreationExpectations();
+
+        final GitHubPRSkipMatchNumber event = new GitHubPRSkipMatchNumber(null);
+
+        final GitHubPRCause cause = event.check(null, remotePr, null, listener);
+        assertThat(cause, notNullValue());
+        assertThat(cause.isSkip(), is(true));
+    }
+
+
+    private void commonExpectations() throws IOException {
+        when(localPR.getLabels()).thenReturn(Collections.<String>emptySet());
+        when(remotePr.getState()).thenReturn(GHIssueState.OPEN);
+        when(remotePr.getRepository()).thenReturn(repository);
+        when(repository.getIssue(anyInt())).thenReturn(issue);
+        when(listener.getLogger()).thenReturn(logger);
+    }
+
+    private void causeCreationExpectations() throws IOException {
+        when(remotePr.getUser()).thenReturn(mockUser);
+        when(remotePr.getHead()).thenReturn(mockPointer);
+        when(remotePr.getBase()).thenReturn(mockPointer);
+    }
+}
