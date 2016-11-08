738ab13b0b4293790a209d00f68554306227ca73
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-11-08 21:56:14 +0300

    checkstyle
    Signed-off-by: Kanstantsin Shautsou <kanstantsin.sha@gmail.com>

--- github-pullrequest-plugin/src/main/java/com/github/kostyasha/github/integration/branch/dsl/GitHubBranchJobDslExtenstion.java
+++ github-pullrequest-plugin/src/main/java/com/github/kostyasha/github/integration/branch/dsl/GitHubBranchJobDslExtenstion.java
@@ -3,9 +3,6 @@ package com.github.kostyasha.github.integration.branch.dsl;
 import antlr.ANTLRException;
 import com.github.kostyasha.github.integration.branch.GitHubBranchTrigger;
 import com.github.kostyasha.github.integration.branch.dsl.context.GitHubBranchTriggerDslContext;
-
-import org.jenkinsci.plugins.github.pullrequest.restrictions.GitHubPRBranchRestriction;
-
 import hudson.Extension;
 import javaposse.jobdsl.dsl.helpers.triggers.TriggerContext;
 import javaposse.jobdsl.plugin.ContextExtensionPoint;
