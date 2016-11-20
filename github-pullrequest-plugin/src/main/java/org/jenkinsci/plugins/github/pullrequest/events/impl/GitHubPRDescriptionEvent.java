3873bc530764b924f2108e7983c7ffe6e4857f48
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-11-20 21:27:48 +0200

    Test and logging for description (body) matcher. (#168)
    * Test and logging for description (body) matcher.
    
    Related to #98
    
    Signed-off-by: Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
    
    * Add test case
    
    Signed-off-by: Kanstantsin Shautsou <kanstantsin.sha@gmail.com>

--- github-pullrequest-plugin/src/main/java/org/jenkinsci/plugins/github/pullrequest/events/impl/GitHubPRDescriptionEvent.java
+++ github-pullrequest-plugin/src/main/java/org/jenkinsci/plugins/github/pullrequest/events/impl/GitHubPRDescriptionEvent.java
@@ -11,7 +11,8 @@ import org.jenkinsci.plugins.github.pullrequest.events.GitHubPREventDescriptor;
 import org.kohsuke.github.GHPullRequest;
 import org.kohsuke.stapler.DataBoundConstructor;
 
-import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Arrays;
@@ -21,12 +22,14 @@ import org.slf4j.LoggerFactory;
 
 import java.util.regex.Pattern;
 
+import static org.apache.commons.lang3.StringEscapeUtils.escapeJava;
+
 /**
  * Used to skip PR builder. Use case is skipping if special description exists.
  */
 public class GitHubPRDescriptionEvent extends GitHubPREvent {
     private static final String DISPLAY_NAME = "Description matched to pattern";
-    private static final Logger LOGGER = LoggerFactory.getLogger(GitHubPRDescriptionEvent.class);
+    private static final Logger LOG = LoggerFactory.getLogger(GitHubPRDescriptionEvent.class);
 
     private final String skipMsg;
 
@@ -42,27 +45,49 @@ public class GitHubPRDescriptionEvent extends GitHubPREvent {
      */
     @Override
     public GitHubPRCause check(GitHubPRTrigger gitHubPRTrigger,
-                               GHPullRequest remotePR,
-                               @CheckForNull GitHubPRPullRequest localPR,
+                               @Nonnull GHPullRequest remotePR,
+                               @Nullable GitHubPRPullRequest localPR,
                                TaskListener listener) throws IOException {
         final PrintStream logger = listener.getLogger();
 
         GitHubPRCause cause = null;
 
         String pullRequestBody = remotePR.getBody().trim();
+        LOG.debug("Job: '{}', trigger event: '{}', body for test: '{}'",
+                gitHubPRTrigger.getJob().getFullName(),
+                DISPLAY_NAME,
+                escapeJava(pullRequestBody)
+        );
+
         if (StringUtils.isNotBlank(pullRequestBody)) {
             HashSet<String> skipBuildPhrases = new HashSet<>(Arrays.asList(getSkipMsg().split("[\\r\\n]+")));
             skipBuildPhrases.remove("");
+            LOG.debug("Job: '{}', trigger event: '{}', skipBuildPhrases: '{}'",
+                    gitHubPRTrigger.getJob().getFullName(),
+                    DISPLAY_NAME,
+                    skipBuildPhrases
+            );
 
             for (String skipBuildPhrase : skipBuildPhrases) {
                 skipBuildPhrase = skipBuildPhrase.trim();
                 Pattern skipBuildPhrasePattern = Pattern.compile(skipBuildPhrase);
                 if (skipBuildPhrasePattern.matcher(pullRequestBody).matches()) {
-                    LOGGER.info("Pull request description with {} skipBuildPhrase. Hence skipping the buildAndComment.",
-                            skipBuildPhrase);
+                    LOG.info("Job: '{}', trigger event: '{}', PR body '{}' matches skipBuildPhrase '{}'.",
+                            gitHubPRTrigger.getJob().getFullName(),
+                            DISPLAY_NAME,
+                            escapeJava(pullRequestBody),
+                            skipBuildPhrase
+                    );
                     logger.println(DISPLAY_NAME + ": Pull request description contains " + skipBuildPhrase + ", skipping");
                     cause = new GitHubPRCause(remotePR, "Pull request description contains " + skipBuildPhrase + ", skipping", true);
                     break;
+                } else {
+                    LOG.trace("Job: '{}', trigger event: '{}', phrase: '{}' didn't match to '{}'",
+                            gitHubPRTrigger.getJob().getFullName(),
+                            DISPLAY_NAME,
+                            skipBuildPhrase,
+                            escapeJava(pullRequestBody)
+                    );
                 }
             }
         }
