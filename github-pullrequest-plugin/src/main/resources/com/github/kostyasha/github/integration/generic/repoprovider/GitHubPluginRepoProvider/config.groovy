6dfaaf07dfa20617836bd6afeb8696b2f03684f3
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-12-06 21:57:03 +0300

    Cache optionally.
    Signed-off-by: Kanstantsin Shautsou <kanstantsin.sha@gmail.com>

---
+++ github-pullrequest-plugin/src/main/resources/com/github/kostyasha/github/integration/generic/repoprovider/GitHubPluginRepoProvider/config.groovy
@@ -0,0 +1,14 @@
+package com.github.kostyasha.github.integration.generic.repoprovider.GitHubPluginRepoProvider
+
+import com.github.kostyasha.github.integration.generic.repoprovider.GitHubPluginRepoProvider
+
+def f = namespace(FormTagLib);
+def st = namespace("jelly:stapler")
+
+if (instance == null) {
+    instance = new GitHubPluginRepoProvider();
+}
+
+f.entry(title: "Cache connection?", field: "cacheConnection") {
+    f.checkbox(default: false)
+}
