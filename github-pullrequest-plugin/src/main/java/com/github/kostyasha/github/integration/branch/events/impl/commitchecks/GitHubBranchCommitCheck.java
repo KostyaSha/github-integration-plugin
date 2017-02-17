393be607b7d0d10bb3c66f871eba87ccad3ec1ec
Jae Gangemi <jgangemi@gmail.com>
2017-02-17 15:53:03 -0500

    - check the last commit's message if repo isn't known

--- github-pullrequest-plugin/src/main/java/com/github/kostyasha/github/integration/branch/events/impl/commitchecks/GitHubBranchCommitCheck.java
+++ github-pullrequest-plugin/src/main/java/com/github/kostyasha/github/integration/branch/events/impl/commitchecks/GitHubBranchCommitCheck.java
@@ -2,19 +2,26 @@ package com.github.kostyasha.github.integration.branch.events.impl.commitchecks;
 
 import com.github.kostyasha.github.integration.branch.GitHubBranchCause;
 import com.github.kostyasha.github.integration.branch.GitHubBranchRepository;
-
 import com.github.kostyasha.github.integration.branch.events.impl.GitHubBranchCommitEvent;
+
 import hudson.ExtensionPoint;
 import hudson.model.AbstractDescribableImpl;
 
 import org.kohsuke.github.GHBranch;
+import org.kohsuke.github.GHCommit;
 import org.kohsuke.github.GHCompare.Commit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 /**
  * @see GitHubBranchCommitEvent
  */
 public abstract class GitHubBranchCommitCheck extends AbstractDescribableImpl<GitHubBranchCommitCheck>
         implements ExtensionPoint {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GitHubBranchCommitCheck.class);
 
     @Override
     public GitHubBranchCommitCheckDescriptor getDescriptor() {
@@ -30,4 +37,27 @@ public abstract class GitHubBranchCommitCheck extends AbstractDescribableImpl<Gi
      * @return <code>GitHubBranchCause</code> instance indicating if the build should be skipped, <code>null</code> otherwise.
      */
     public abstract GitHubBranchCause check(GHBranch remoteBranch, GitHubBranchRepository localRepo, Commit[] commits);
+
+    /**
+     * Check used to determine if some associated commit property, such as the commit message, should prevent a build from being triggered.
+     * <p>
+     * This method is called when the repository is not yet known to the plugin.
+     * </p>
+     *
+     * @param remoteBranch current branch state from GH.
+     * @param localRepo local repository state.
+     * @param commit last commit in the remote repository.
+     * @return <code>GitHubBranchCause</code> instance indicating if the build should be skipped, <code>null</code> otherwise.
+     */
+    public GitHubBranchCause check(GHBranch remoteBranch, GitHubBranchRepository localRepo, GHCommit commit) {
+        try {
+            return doCheck(remoteBranch, localRepo, commit);
+        } catch (IOException e) {
+            LOG.error("Failed to check commit for hash [{}]", commit.getSHA1(), e);
+            return new GitHubBranchCause(remoteBranch, localRepo, e.getMessage(), true);
+        }
+    }
+
+    protected abstract GitHubBranchCause doCheck(GHBranch remoteBranch, GitHubBranchRepository localRepo, GHCommit commit)
+        throws IOException;
 }
