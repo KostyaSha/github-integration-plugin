9d32ba697a79aacb179811c6a536b5470a0c7b80
nicolacovallero-jibo <nicola.covallero@jibo.com>
2018-03-06 12:00:08 +0100

    Started implementating class to handle the approval of a pull request review

---
+++ github-pullrequest-plugin/src/main/java/org/jenkinsci/plugins/github/pullrequest/events/impl/GitHubPRApproved.java
@@ -0,0 +1,69 @@
+package org.jenkinsci.plugins.github.pullrequest.events.impl;
+
+import hudson.Extension;
+import hudson.model.TaskListener;
+import org.jenkinsci.plugins.github.pullrequest.GitHubPRCause;
+import org.jenkinsci.plugins.github.pullrequest.GitHubPRPullRequest;
+import org.jenkinsci.plugins.github.pullrequest.GitHubPRTrigger;
+import org.jenkinsci.plugins.github.pullrequest.events.GitHubPREvent;
+import org.jenkinsci.plugins.github.pullrequest.events.GitHubPREventDescriptor;
+import org.kohsuke.github.GHIssueState;
+import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GHPullRequestReview;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.jenkinsci.plugins.github.pullrequest.utils.ObjectsUtil.isNull;
+
+/**
+ * When PR is approved
+ *
+ * @author Nicola Covallero
+ */
+public class GitHubPRApproved extends GitHubPREvent {
+    private static final String DISPLAY_NAME = "Pull Request Approved";
+    private static final Logger LOG = LoggerFactory.getLogger(GitHubPRApproved.class); //NOPMD
+    private GHPullRequestReview prr = new GHPullRequestReview();
+
+    @DataBoundConstructor
+    public GitHubPRApproved() {
+    }
+
+    @Override
+    public GitHubPRCause check(GitHubPRTrigger gitHubPRTrigger, GHPullRequest remotePR,
+                               GitHubPRPullRequest localPR, TaskListener listener) throws IOException {
+        if (isNull(localPR)) {
+            return null;
+        }
+
+        GitHubPRCause cause = null;
+
+        // must be closed once
+        // if (remotePR.getState().equals(GHIssueState.CLOSED)) {
+        //     final PrintStream logger = listener.getLogger();
+        //     logger.println(DISPLAY_NAME + ": state has changed (PR was closed)");
+        //     cause = new GitHubPRCause(remotePR, "PR was accept", false);
+        // }
+
+        // must be approved once per reviewer
+        if (remotePR.getState().equals(GHIssueState.CLOSED)) {
+            final PrintStream logger = listener.getLogger();
+            logger.println(DISPLAY_NAME + ": state has changed (PR was approved)");
+            cause = new GitHubPRCause(remotePR, "PR was approved", false);
+        }
+
+        return cause;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends GitHubPREventDescriptor {
+        @Override
+        public String getDisplayName() {
+            return DISPLAY_NAME;
+        }
+    }
+}
