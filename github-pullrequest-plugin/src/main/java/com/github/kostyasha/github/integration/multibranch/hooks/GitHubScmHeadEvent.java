121fec3f68d6e70031e7de1f835aac03af50128c
Anton Tanasenko <atg.sleepless@gmail.com>
2018-03-22 20:11:52 +0200

    Cleanup scmEvents

--- github-pullrequest-plugin/src/main/java/com/github/kostyasha/github/integration/multibranch/hooks/GitHubScmHeadEvent.java
+++ github-pullrequest-plugin/src/main/java/com/github/kostyasha/github/integration/multibranch/hooks/GitHubScmHeadEvent.java
@@ -1,7 +1,56 @@
 package com.github.kostyasha.github.integration.multibranch.hooks;
 
+import javax.annotation.Nonnull;
+
+import com.cloudbees.jenkins.GitHubRepositoryName;
+import com.github.kostyasha.github.integration.multibranch.GitHubSCMSource;
+
+import hudson.scm.SCM;
+import jenkins.scm.api.SCMHeadEvent;
+import jenkins.scm.api.SCMNavigator;
+import jenkins.scm.api.SCMSource;
+
 /**
  * @author Kanstantsin Shautsou
  */
-public class GitHubScmHeadEvent {
+public abstract class GitHubScmHeadEvent<T> extends SCMHeadEvent<T> {
+
+    protected GitHubScmHeadEvent(Type type, long timestamp, T payload, String origin) {
+        super(type, timestamp, payload, origin);
+    }
+
+    @Nonnull
+    protected abstract String getSourceRepo();
+
+    @Override
+    public boolean isMatch(SCMSource source) {
+        if (!(source instanceof GitHubSCMSource)) {
+            return false;
+        }
+        return getSourceRepo().equals(getSourceRepo(source));
+    }
+
+    @Nonnull
+    @Override
+    public String getSourceName() {
+        return getSourceRepo();
+    }
+
+    @Nonnull
+    protected String getSourceRepo(SCMSource source) {
+        GitHubSCMSource gitHubSCMSource = (GitHubSCMSource) source;
+        String projectUrlStr = gitHubSCMSource.getProjectUrlStr();
+        GitHubRepositoryName repo = GitHubRepositoryName.create(projectUrlStr);
+        return String.format("%s/%s", repo.getUserName(), repo.getRepositoryName());
+    }
+
+    @Override
+    public boolean isMatch(SCMNavigator navigator) {
+        return false;
+    }
+
+    @Override
+    public boolean isMatch(SCM scm) {
+        return false;
+    }
 }
