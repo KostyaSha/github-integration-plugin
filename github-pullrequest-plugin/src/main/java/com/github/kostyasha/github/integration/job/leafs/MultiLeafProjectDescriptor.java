e27d7cb75bedc80fe6c1aa1a74e676b4fd549da9
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2018-07-01 04:00:43 +0300

    Remove gargabe classes.
    Were used during multibranch PoC.

--- github-pullrequest-plugin/src/main/java/com/github/kostyasha/github/integration/job/leafs/MultiLeafProjectDescriptor.java
+++
@@ -1,7 +0,0 @@
-package com.github.kostyasha.github.integration.job.leafs;
-
-import com.cloudbees.hudson.plugins.folder.AbstractFolderDescriptor;
-
-public abstract class MultiLeafProjectDescriptor extends AbstractFolderDescriptor {
-
-}
