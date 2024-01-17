bc4135717e32079308aa2be4052a09a5ed45b3ce
Basil Crow <me@basilcrow.com>
2024-01-17 15:48:16 -0800

    Forward compatibility with Guice 7

--- github-pullrequest-plugin/src/test/java/com/github/kostyasha/github/integration/branch/GitHubBranchTriggerTest.java
+++ github-pullrequest-plugin/src/test/java/com/github/kostyasha/github/integration/branch/GitHubBranchTriggerTest.java
@@ -38,7 +38,7 @@ import org.jvnet.hudson.test.recipes.LocalData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.inject.Inject;
+import jakarta.inject.Inject;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
